19c009de66802fea70f365b13c4aa0a3bb22b31a
Ankur Garg <agarg12@ncsu.edu>
2017-10-18 01:53:47 -0400

    passing perspective and model view matrices to shaders

--- Program2/rasterize.js
+++ Program2/rasterize.js
@@ -10,6 +10,10 @@ var Eye = new vec4.fromValues(0.5,0.5,-0.5,1.0); // default eye position in worl
 
 /* webgl globals */
 var gl = null; // the all powerful gl object. It's all here folks!
+var shaderProgram;
+var mvmatrix = mat4.create();   //ModelView Transformation Matrix
+var pmatrix = mat4.create();    //Projection Matrix
+
 var ambientColorBuffer; // this contains vertex colors in triples
 var diffuseColorBuffer; // this contains vertex colors in triples
 var specularColorBuffer; // this contains vertex colors in triples
@@ -32,6 +36,12 @@ var vertexNormalAttrib; // where to put normals for each vertex
 var vertexAmbientColorAttrib; // where to put color for vertex shader
 var vertexDiffuseColorAttrib; // where to put color for vertex shader
 var vertexSpecularColorAttrib; // where to put color for vertex shader
+
+var LightPos = [-1, 3, -0.5];
+var LightColor = [1.0, 1.0, 1.0];
+var Eye = [0.5, 0.5, -0.5];
+var Up = [0, 1, 0];
+var Center = [0.5, 0.5, 1];
 
 
 // ASSIGNMENT HELPER FUNCTIONS
@@ -287,12 +297,15 @@ function setupShaders() {
         varying vec3 vNormal;
         varying vec4 vPosition;
 
+        uniform vec3 lightPos;
+        uniform vec3 lightColor;
+        uniform vec3 eyePos;
+
         void main(void) {
-            vec3 lightWeighting;
-            vec3 lightDirection = normalize(vec3(-1, 3, -0.5) - vPosition.xyz);
+            vec3 lightDirection = normalize(lightPos - vPosition.xyz);
             
             vec3 normal = normalize(vNormal);
-            vec3 eyeDirection = normalize(vec3(0.5, 0.5, -0.5)-vPosition.xyz);
+            vec3 eyeDirection = normalize(eyePos - vPosition.xyz);
             vec3 reflectionDirection = reflect(-lightDirection, normal);
 
             float specularLightWeighting = pow(max(dot(reflectionDirection, eyeDirection), 0.0), vSpecularColor.a);
@@ -300,11 +313,9 @@ function setupShaders() {
 
             float directionalLightWeighting = max(dot(normal, lightDirection), 0.0);
 
-            lightWeighting = vec3(1.0, 1.0, 1.0);
-
-            gl_FragColor = vec4(vAmbientColor.rgb * lightWeighting, vAmbientColor.a) + 
-                        vec4(vDiffuseColor.rgb * lightWeighting * directionalLightWeighting, vDiffuseColor.a) + 
-                        vec4(vSpecularColor.rgb * lightWeighting * specularLightWeighting, 1.0);
+            gl_FragColor = vec4(vAmbientColor.rgb * lightColor, vAmbientColor.a) + 
+                        vec4(vDiffuseColor.rgb * lightColor * directionalLightWeighting, vDiffuseColor.a) + 
+                        vec4(vSpecularColor.rgb * lightColor * specularLightWeighting, 1.0);
 
         }
     `;
@@ -323,13 +334,18 @@ function setupShaders() {
         varying vec3 vNormal;
         varying vec4 vPosition;
 
+        uniform mat4 MVMatrix;
+        uniform mat4 PMatrix;
+        uniform mat3 NMatrix;
+
         void main(void) {
-            gl_Position = vec4(vertexPosition, 1.0); // use the untransformed position
+            gl_Position = MVMatrix * vec4(vertexPosition, 1.0); // use the untransformed position
+            vPosition = gl_Position;
+            gl_Position = PMatrix * vPosition;
             vAmbientColor = aVertexAmbientColor;
             vDiffuseColor = aVertexDiffuseColor;
             vSpecularColor = aVertexSpecularColor;
-            vNormal = vertexNormal;
-            vPosition = gl_Position;
+            vNormal = NMatrix * vertexNormal;
         }
     `;
     
@@ -351,7 +367,7 @@ function setupShaders() {
             throw "error during vertex shader compile: " + gl.getShaderInfoLog(vShader);  
             gl.deleteShader(vShader);
         } else { // no compile errors
-            var shaderProgram = gl.createProgram(); // create the single shader program
+            shaderProgram = gl.createProgram(); // create the single shader program
             gl.attachShader(shaderProgram, fShader); // put frag shader in program
             gl.attachShader(shaderProgram, vShader); // put vertex shader in program
             gl.linkProgram(shaderProgram); // link program into gl context
@@ -374,6 +390,13 @@ function setupShaders() {
 
                 vertexSpecularColorAttrib = gl.getAttribLocation(shaderProgram, "aVertexSpecularColor");
                 gl.enableVertexAttribArray(vertexSpecularColorAttrib); // input to shader from array
+
+                shaderProgram.pmatrix = gl.getUniformLocation(shaderProgram, "PMatrix");
+                shaderProgram.mvmatrix = gl.getUniformLocation(shaderProgram, "MVMatrix");
+                shaderProgram.nmatrix = gl.getUniformLocation(shaderProgram, "NMatrix");
+                shaderProgram.lightPos = gl.getUniformLocation(shaderProgram, "lightPos");
+                shaderProgram.lightColor = gl.getUniformLocation(shaderProgram, "lightColor");
+                shaderProgram.eyePos = gl.getUniformLocation(shaderProgram, "eyePos");
             } // end if no shader program link errors
         } // end if no compile errors
     } // end try 
@@ -389,8 +412,27 @@ function renderStuff() {
     renderEllipsoids();
 }
 
+function setupLights() {
+    gl.uniform3f(shaderProgram.lightPos, LightPos[0], LightPos[1], LightPos[2]);
+    gl.uniform3f(shaderProgram.lightColor, LightColor[0], LightColor[1], LightColor[2]);
+    gl.uniform3f(shaderProgram.eyePos, Eye[0], Eye[1], Eye[2]);
+
+    gl.uniformMatrix4fv(shaderProgram.pmatrix, false, pmatrix);
+    gl.uniformMatrix4fv(shaderProgram.mvmatrix, false, mvmatrix);
+    var nmatrix = mat3.create();
+    mat3.fromMat4(nmatrix, mvmatrix);
+    mat3.invert(nmatrix, nmatrix);
+    mat3.transpose(nmatrix, nmatrix);
+    gl.uniformMatrix3fv(shaderProgram.nmatrix, false, nmatrix);
+
+}
+
 // render the loaded model
 function renderTriangles() {
+
+    mat4.perspective(pmatrix, 90*Math.PI/180, 1.0, 0.5, 2.0);
+    mat4.lookAt(mvmatrix, Eye, Center, Up);
+
     // vertex buffer: activate and feed into vertex shader
     gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer); // activate
     gl.vertexAttribPointer(vertexPositionAttrib, vertexBuffer.itemSize, gl.FLOAT, false, 0, 0); // feed
@@ -410,6 +452,7 @@ function renderTriangles() {
 
     // triangle buffer: activate and render
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,triangleBuffer); // activate
+    setupLights();
     gl.drawElements(gl.TRIANGLES,triBufferSize,gl.UNSIGNED_SHORT,0); // render
 } // end render triangles
 
@@ -433,6 +476,7 @@ function renderEllipsoids() {
 
     // ellipsoid buffer: activate and render
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ellipsiodsVertexIndexBuffer); // activate
+    setupLights();
     gl.drawElements(gl.TRIANGLES,ellipsiodsVertexIndexBuffer.numItems,gl.UNSIGNED_SHORT,0); // render
 }
 
